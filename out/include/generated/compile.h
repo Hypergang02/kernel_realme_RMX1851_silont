2201c4f5fec752bbf0dcd79fce6b32a3f9bcefcf
akufarish <risfarishk@gmail.com>
2023-12-17 20:28:28 +0800

    add powersave thanks to mrkribo

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Dec 17 20:19:43 WITA 2023"
+#define LINUX_COMPILE_BY "akufarish"
+#define LINUX_COMPILE_HOST "LAPTOP-NOC7STBR"
+#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a), LLD 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a)"
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.9.248-SilonT-Morgan"
