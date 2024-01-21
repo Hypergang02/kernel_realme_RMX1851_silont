0db4f128a12a9de0499d8fecd03b41ecdb30afdf
akufarish <risfarishk@gmail.com>
2024-01-21 11:43:39 +0800

    a

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Jan 16 20:41:26 WITA 2024"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jan 17 13:44:26 WITA 2024"
 #define LINUX_COMPILE_BY "AkuFarish"
 #define LINUX_COMPILE_HOST "Virtuosa"
 #define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a), LLD 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a)"
